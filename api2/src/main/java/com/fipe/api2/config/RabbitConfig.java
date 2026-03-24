c2736034ea6cd811a274b0fe52d214496c12aba1
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-23 22:28:10 -0300

    dev (api2): defines RabbitConfig

---
+++ api2/src/main/java/com/fipe/api2/config/RabbitConfig.java
@@ -0,0 +1,25 @@
+package com.fipe.api2.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableRabbit
+@Configuration
+public class RabbitConfig {
+
+  @Bean
+  public Queue queue() {
+    return QueueBuilder.durable("brand.queue").build();
+  }
+
+  @Bean
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+}
