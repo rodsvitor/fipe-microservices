0fc501396d13aa8b4c708195c55f09d16130a70b
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-25 15:45:38 -0300

    dev (api1): adds some TODOs

--- api1/src/main/java/com/fipe/api1/Api1Application.java
+++ api1/src/main/java/com/fipe/api1/Api1Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Api1Application {
 
+	// TODO TESTES
+	// ‼️ TODO PAGEABLE
+	// TODO UPDATE README,SEE THE INSTRUCTIONS FIRST
+
 	public static void main(String[] args) {
 		SpringApplication.run(Api1Application.class, args);
 	}
