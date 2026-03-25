d9b836246f9e3f60246b3899567ac016e53a9b5e
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-25 15:38:23 -0300

    dev (api1): defines exception for serialization from Jackson

---
+++ api1/src/main/java/com/fipe/api1/infrastructure/exception/SerializationException.java
@@ -0,0 +1,7 @@
+package com.fipe.api1.infrastructure.exception;
+
+public class SerializationException extends RuntimeException {
+  public SerializationException(Throwable cause) {
+    super(cause);
+  }
+}
