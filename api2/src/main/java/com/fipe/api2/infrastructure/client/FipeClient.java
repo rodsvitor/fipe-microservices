d9702cde04cf300097cd48ee64f3088c2f302936
rodsvitor <rodrigo.rvsouza@gmail.com>
2026-03-25 13:05:54 -0300

    dev (api2): refactors FipeClient considering the category

--- api2/src/main/java/com/fipe/api2/infrastructure/client/FipeClient.java
+++ api2/src/main/java/com/fipe/api2/infrastructure/client/FipeClient.java
@@ -1,8 +1,8 @@
 package com.fipe.api2.infrastructure.client;
 
+import com.fipe.api2.domain.model.Category;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -13,23 +13,29 @@ public class FipeClient {
 
   private final RestTemplate restTemplate;
 
-  public FipeModelResponse getModelsByBrand(Long brandId) {
+  @Value("${fipe-api.base-url}")
+  private String fipeApiUrl;
 
-    String url = "https://parallelum.com.br/fipe/api/v1/carros/marcas/" + brandId + "/modelos";
-    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4NzkzNmM2YS01NjU5LTQ1YzEtODFmZi02ODc4ZjdjNDhiMDMiLCJlbWFpbCI6InJvZHJpZ28ucnZzb3V6YUBnbWFpbC5jb20iLCJpYXQiOjE3NzQxNDU1MDB9.oxf8e6aEyVrUsAm1EL39GDiDz-KXJIZqBtipf6KQ798";
+  public FipeModelResponse getModelsByBrandId(Category category, Long brandId) {
 
-    var headers = new HttpHeaders();
-    headers.setBearerAuth(token);
-
-    var entity = new HttpEntity<>(headers);
+    String url = fipeApiUrl + getCategoryResource(category, brandId);
 
     return restTemplate.exchange(
         url,
         HttpMethod.GET,
-        entity,
+        null,
         FipeModelResponse.class
     ).getBody();
+  }
 
+  private static String getCategoryResource(Category category, Long brandId) {
+    var resource = switch (category) {
+      case CAR -> "/carros/marcas";
+      case MOTORCYCLE -> "/motos/marcas";
+      case TRUCK -> "/caminhoes/marcas";
+    };
+
+    return resource + "/" + brandId + "/modelos";
   }
 
 }
